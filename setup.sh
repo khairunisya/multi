54545efabf6f5f3c513142e042119384121ee621
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-08 03:59:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -225,11 +225,11 @@ function install_requirement() {
     sed -i -e 's/\r$//' xp-vmess
     cd
 
-    echo "0 0 * * * root reboot" >> /etc/crontab
-    echo "0 0 * * * root xp-tr" >> /etc/crontab
+    echo "0 0 * * * root clearlog && reboot" >> /etc/crontab
+    #echo "0 0 * * * root xp-tr" >> /etc/crontab
     #echo "0 0 * * * root xp-ss" >> /etc/crontab
-    echo "0 0 * * * root xp-vless" >> /etc/crontab
-    echo "0 0 * * * root xp-vmess" >> /etc/crontab
+    #echo "0 0 * * * root xp-vless" >> /etc/crontab
+    #echo "0 0 * * * root xp-vmess" >> /etc/crontab
     cd
 
     mkdir /home/trojan
