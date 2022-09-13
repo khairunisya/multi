6eda973e0e8eb5251b887ee4906001ed4b5e6116
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-09-14 01:42:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -62,6 +62,46 @@ function install_requirement() {
     apt autoremove -y
     apt clean -y
 
+    # install wget and curl
+apt -y install wget curl
+apt -y install net-tools
+
+# Install Requirements Tools
+apt install ruby -y
+apt install python -y
+apt install make -y
+apt install cmake -y
+apt install coreutils -y
+apt install rsyslog -y
+apt install net-tools -y
+apt install zip -y
+apt install unzip -y
+apt install nano -y
+apt install sed -y
+apt install gnupg -y
+apt install gnupg1 -y
+apt install bc -y
+apt install jq -y
+apt install apt-transport-https -y
+apt install build-essential -y
+apt install dirmngr -y
+apt install libxml-parser-perl -y
+apt install neofetch -y
+apt install git -y
+apt install lsof -y
+apt install libsqlite3-dev -y
+apt install libz-dev -y
+apt install gcc -y
+apt install g++ -y
+apt install libreadline-dev -y
+apt install zlib1g-dev -y
+apt install libssl-dev -y
+apt install libssl1.0-dev -y
+apt install dos2unix -y
+
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
     #  Menghapus apache2 nginx sendmail ufw firewall dan exim4 untuk menghindari port nabrak
     apt remove --purge nginx apache2 sendmail ufw firewalld exim4 -y >/dev/null 2>&1
     apt autoremove -y
