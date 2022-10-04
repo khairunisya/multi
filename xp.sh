231a36a838064ed4afeadf858d6e2852cf63b5cc
Muhammad Irfan <106350325+khairunisya@users.noreply.github.com>
2022-10-05 02:44:29 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -72,17 +72,3 @@ rm -f /home/vps/public_html/ss-ws-${user}.txt
 fi
 done
 systemctl restart xray@tls
-data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### Client $user" "/etc/wireguard/wg0.conf" | cut -d ' ' -f 4)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### Client $user $exp/,/^AllowedIPs/d" /etc/wireguard/wg0.conf
-rm -f "/var/www/html/$user.conf"
-fi
-done
-systemctl restart wg-quick@wg0
